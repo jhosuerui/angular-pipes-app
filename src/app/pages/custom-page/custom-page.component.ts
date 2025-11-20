fa265954f7a27d7148bbfd220d32ffffa56bca0c
Jhosue Rui <ingrui@gmail.com>
2025-11-20 12:21:16 -0400

    color style

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -2,10 +2,17 @@ import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { Color, ColorMap } from '../../interfaces/hero';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe,CanFlyPipe],
+  imports: [ToggleCasePipe,
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -15,7 +22,12 @@ export default class CustomPageComponent {
 
   heroes =signal(heroes);
 
+
   changeCase(){
     this.toggle.set(!this.toggle());
+  }
+
+  colorValue(color:Color){
+    return ColorMap[color];
   }
 }
